a617974c2c6fbd14b05dc9c7d9ce5b7e816a641e
Kevin-KHA <201378@ids.upchiapas.edu.mx>
2021-07-18 21:28:36 -0500

    Boton de menu configurado

--- src/main/view/menuController.java
+++ src/main/view/menuController.java
@@ -3,6 +3,11 @@ package main.view;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 
@@ -33,7 +38,48 @@ public class menuController {
     private Label lblfinanzas1;
 
     @FXML
+    private Button cerrarCategorias;
+
+    @FXML
+    private TableView<?> listaCategoria;
+
+    @FXML
+    private TableColumn<?, ?> colClasificacion;
+
+    @FXML
+    private TableColumn<?, ?> colCategoria;
+
+    @FXML
+    private TableColumn<?, ?> colSubCat;
+
+    @FXML
     private Label lblfinanzas11;
+
+    @FXML
+    private TextField catSeleccionada;
+
+    @FXML
+    private TextField subCatSeleccionada;
+
+    @FXML
+    private MenuButton menuClas;
+
+    @FXML
+    private MenuItem setGAO;
+
+    @FXML
+    private MenuItem setIngreso;
+
+    @FXML
+    private MenuItem setCostoVeta;
+
+    @FXML
+    private Button guardarCategoria;
+
+    @FXML
+    void cerrarCategoria() {
+        menuCategoria.setVisible(false);
+    }
 
     @FXML
     void iniciarCategoria() {
